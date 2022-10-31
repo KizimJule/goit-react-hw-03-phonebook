ed6031b605ead211c73ebc985e74fa14ed2aefb7
KizimJule <kizimjule@gmail.com>
2022-10-31 10:24:54 +0100

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,7 +43,13 @@ export class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
-
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
   componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
